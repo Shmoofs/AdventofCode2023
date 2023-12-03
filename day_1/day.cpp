5ea1ad5e944a601583250df454eb8c265092da2c
Tim A <timothyalsaudi@gmail.com>
2023-12-02 17:13:12 -0800

    Finished Day 1: Part 2

--- day_1/day.cpp
+++ day_1/day.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <vector>
+#include <algorithm>
+#include <cctype>
+
+std::string ReplaceAll(std::string str, const std::string& from, const std::string& to) {
+    size_t start_pos = 0;
+    while((start_pos = str.find(from, start_pos)) != std::string::npos) {
+        str.replace(start_pos, from.length(), to);
+        start_pos += to.length();
+    }
+    return str;
+}
 
 int main() {
     std::ifstream inputFile("input.txt");
@@ -10,26 +22,28 @@ int main() {
     }
 
     int total = 0;
-    int number = 0;
-    
     std::string line;
     std::string strnum = "00";
+    std::string all_digits[9] = {"o1e", "t2o", "t3e", "f4r", "f5e", "s6x", "s7n", "e8t", "n9e"};
+    std::string all_digits_string[9] = {"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
+    
     while (std::getline(inputFile, line)) {
        std::string digits;
-        for (char c : line) {
-            if (std::isdigit(c)) {
-                digits += c;
-            }
+        for (int i = 0; i < 9; i++) {
+           line = ReplaceAll(line, all_digits_string[i], all_digits[i]);
         }
+
+        for(char c : line){
+            if (isdigit(c)) digits += c;
+        }
+
         if(digits.length() > 0) {
             strnum[0] = digits[0];
             strnum[1] = digits[digits.length() - 1];
-            total += stoi(strnum);
-            }
+            total += std::stoi(strnum);
         }
-    
-    std::cout << "Total: " << total << std::endl;
-    inputFile.close();
+    }
+    std::cout << total << std::endl;
     return 0;
 }
 
