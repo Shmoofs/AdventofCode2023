1591835bbe515d8b761a744f000930ef27543cf4
Tim A <timothyalsaudi@gmail.com>
2023-12-13 14:18:07 -0800

    Finished day 2 part 2

--- day_2/day2.cpp
+++ day_2/day2.cpp
@@ -14,15 +14,29 @@ void countOccurances(std::string nextWord, std::string word, int count[3]) {
     count[2] += std::stoi(word);
 }
 
+void countOccurancesMax(std::string nextWord, std::string word, int count[3]) {
+    if (nextWord.find("red") != std::string::npos 
+    && count[0] < std::stoi(word)){
+    count[0] = std::stoi(word);
+    }
 
-int main() {
+    if (nextWord.find("green") != std::string::npos 
+    && count[1] < std::stoi(word)){
+        count[1] = std::stoi(word);
+    }
+    
+    if (nextWord.find("blue") != std::string::npos 
+    && count[2] < std::stoi(word)){
+    count[2] = std::stoi(word);
+    }
+}
+
+void part_1(){
     std::ifstream inputFile("input.txt");
     if (!inputFile) {
         std::cerr << "Failed to open input file." << std::endl;
-        return 1;
+        return;
     }
-
-    int total = 0;
     std::string nextWord;
     int gameCount = 0;
     int possibleGames = 0;
@@ -57,5 +71,37 @@ int main() {
         }
     }
     std::cout << possibleGames << std::endl;
+}
+
+void part_2(){
+    std::ifstream inputFile("input.txt");
+    if (!inputFile) {
+        std::cerr << "Failed to open input file." << std::endl;
+        return;
+    }
+    int total = 0;
+    std::string nextWord;
+    //Red, Green, Blue
+    int count[3] = {0, 0, 0};
+    std::string word;
+
+    while (word = nextWord, inputFile >> nextWord) {
+        if (word.find("Game") != std::string::npos) {
+            total += count[0] * count[1] * count[2];
+            count[0] = 0;
+            count[1] = 0;
+            count[2] = 0; 
+        }
+        countOccurancesMax(nextWord, word, count);
+    }
+    total += count[0] * count[1] * count[2];
+    std::cout << total << std::endl;
+}
+
+int main() {
+
+    part_1();
+    part_2();
+
     return 0;
 }
